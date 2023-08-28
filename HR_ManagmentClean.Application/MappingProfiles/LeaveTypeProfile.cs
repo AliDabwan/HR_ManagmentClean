ddf00cfc073f7776882b9768ed766d014259c764
AliDabwan <dev.alidabwan@gmail.com>
2023-08-28 09:04:23 +0300

    create LeaveTypeQueryHandler

---
+++ HR_ManagmentClean.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HR_ManagmentClean.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using HR_ManagmentClean.Domin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_ManagmentClean.Application.MappingProfiles
+{
+    public class LeaveTypeProfile:Profile
+    {
+        public LeaveTypeProfile()
+        {
+            CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
+        }
+    }
+}
